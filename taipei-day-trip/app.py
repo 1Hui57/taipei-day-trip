9ce4b8a455040a34bb454d44819ae0c28a000b3d
1Hui57 <melody870507@gmail.com>
2025-03-11 21:44:01 +0800

    feat:優化/api/attractions程式碼，完成/api/attraction/{attractionId}路由設置

--- taipei-day-trip/app.py
+++ taipei-day-trip/app.py
@@ -14,37 +14,49 @@ con=mysql.connector.connect(
     database="taipei_day_trip"
 )
 
-# 將要求字串的輸入錯誤以statua=500回應
-@app.exception_handler(RequestValidationError)
-async def validation_exception_handler(request:Request, exc:RequestValidationError):
-	return JSONResponse(status_code=500,content={"error":True,"message":"請輸入大於等於0的整數作為查詢頁數。"})
+# 優化/api/attractions
+# 定義一個函式，可以根據要求字串抓取資料
+def getAttractionData(cursor, keyword=None, pageStartFrom=0, attractionId=None):
+	if keyword:
+		cursor.execute("SELECT * FROM attractions WHERE name=%s OR mrt LIKE %s ORDER BY id ASC LIMIT %s, %s",[keyword,'%'+keyword+'%',pageStartFrom,12])
+	elif attractionId:
+		cursor.execute("SELECT * FROM attractions WHERE id=%s",[attractionId])
+	else:
+		cursor.execute("SELECT * FROM attractions ORDER BY id ASC LIMIT %s, %s",[pageStartFrom,12])
+	return cursor.fetchall()
 
+# 定義一個函式，可以根據要求字串抓取資料總筆數
+def getAttractionNumber(cursor, keyword):
+	if keyword:
+		cursor.execute("SELECT COUNT(*) FROM  attractions WHERE name=%s OR mrt LIKE %s",[keyword, '%'+keyword+'%'])
+	else:
+		cursor.execute("SELECT COUNT(id) FROM  attractions")
+	return cursor.fetchone()
+
+# 定義一個函式，可以抓取圖片url
+def getAttractionImg(cursor,attractions_id):
+	cursor.execute("SELECT url FROM attractions_images WHERE attractions_id=%s",[attractions_id])
+	resultsUrls=cursor.fetchall()# 拿到的url資料會是tuple形式
+	imageUrl= [url[0] for url in resultsUrls] # 將url資料轉換為list
+	return imageUrl
+	 
 @app.get("/api/attractions")
 async def api_attractions(
 	request: Request,
 	page:Annotated[int, Query(ge=0)],
 	keyword:Annotated[Optional[str], Query()]=None
 	):
-	# 找出keyword相關的資料，同時驗證page是否為大於等於0的整數
-	if keyword and type(page)==int and page>=0:
-		cursor=con.cursor()
-		# 根據page算出查詢過後的起始筆數
-		pageStartFrom=page*11 if page>0 else 0
-		# 根據keyword及page獲得該頁的所有資料
-		cursor.execute("SELECT * FROM attractions WHERE name=%s OR mrt LIKE %s ORDER BY id ASC LIMIT %s, %s",[keyword,'%'+keyword+'%',pageStartFrom,12])
-		results=cursor.fetchall()
-		# 根據keyword獲得符合關鍵字的資料總筆數
-		cursor.execute("SELECT COUNT(*) FROM  attractions WHERE name=%s OR mrt LIKE %s",[keyword, '%'+keyword+'%'])
-		resultsNumber=cursor.fetchone() 
-		if results: # 如果根據page跟keyword有查到相關資料
-			data=[]
-			for item in results:
-				# 查詢相同id的圖片url
-				cursor=con.cursor()
-				cursor.execute("SELECT url FROM attractions_images WHERE attractions_id=%s",[item[0]])
-				resultsUrls=cursor.fetchall()# 拿到的url資料會是tuple形式
-				images=[url[0] for url in resultsUrls] # 將url資料轉換為list
-				data.append({
+	cursor=con.cursor()
+	# 根據page算出查詢過後的起始筆數
+	pageStartFrom=page*12 if page>0 else 0
+	results=getAttractionData(cursor, keyword, pageStartFrom)
+	resultsNumber=getAttractionNumber(cursor, keyword)
+	if results:
+		data=[]
+		for item in results:
+			cursor=con.cursor()
+			images=getAttractionImg(cursor,item[0])
+			data.append({
 					"id":item[0],
 					"name":item[1],
 					"category":item[2],
@@ -56,51 +68,42 @@ async def api_attractions(
 					"lng":item[8],
 					"images":images
 					})
-			# 計算結果有幾頁
-			totalPages=math.ceil(resultsNumber[0]/12)-1 #  如果有三頁，是P0,P1,P2頁
-			nextPage=page+1 if totalPages-page>0 else None
-			return {"nextPage":nextPage,"data":results}
-		else:
-			return JSONResponse(status_code=500,content={"error":True,"message":"您所查詢的關鍵字及頁數無相關資料。"})
-	# 如果沒有keyword，但page是大於等於0的整數
-	elif keyword is None and type(page)==int and page>=0 :
-		pageStartFrom=page*12 if page>0 else 0
-		cursor=con.cursor()
-		cursor.execute("SELECT * FROM attractions ORDER BY id ASC LIMIT %s, %s",[pageStartFrom,12])
-		results=cursor.fetchall()
-		# 取得資料總筆數
-		cursor.execute("SELECT COUNT(id) FROM  attractions")
-		resultsNumber=cursor.fetchone() 
-		if results: # 如果根據page有查到相關資料
-			data=[]
-			for item in results:
-				# 查詢相同id的圖片url
-				cursor=con.cursor()
-				cursor.execute("SELECT url FROM attractions_images WHERE attractions_id=%s",[item[0]])
-				resultsUrls=cursor.fetchall()# 拿到的url資料會是tuple形式
-				images=[url[0] for url in resultsUrls] # 將url資料轉換為list
-				data.append({
-					"id":item[0],
-					"name":item[1],
-					"category":item[2],
-					"description":item[3],
-					"address":item[4],
-					"transport":item[5],
-					"mrt":item[6],
-					"lat":item[7],
-					"lng":item[8],
-					"images":images
-					})
-			# 計算結果有幾頁
-			totalPages=math.ceil(resultsNumber[0]/12)-1 #  如果有三頁，是P0,P1,P2頁
-			nextPage=page+1 if totalPages-page>0 else None
-			return {"nextPage":nextPage,"data":results}
-		else:
-			return JSONResponse(status_code=500,content={"error":True,"message":"您所查詢的頁數無相關資料。"})
+		# 計算結果有幾頁
+		totalPages=math.ceil(resultsNumber[0]/12)-1 #  如果有三頁，是P0,P1,P2頁
+		nextPage=page+1 if totalPages-page>0 else None
+		return {"nextPage":nextPage,"data":data}
+	elif keyword:
+		return JSONResponse(status_code=500,content={"error":True,"message":"您所查詢的關鍵字無相關頁數資料。"})
 	else:
-		return JSONResponse(status_code=500,content={"error":True,"message":"請輸入大於等於0的整數作為查詢頁數。"})
-		
-		
+		return JSONResponse(status_code=500,content={"error":True,"message":"您所查詢的頁數無相關資料。"})
+
+# 將要求字串和路徑的輸入錯誤以statua=500回應
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request:Request, exc:RequestValidationError):
+	return JSONResponse(status_code=500,content={"error":True,"message":"請輸入正確的查詢資料。"})
+
+
+@app.get("/api/attraction/{attractionId}")		
+async def api_attraction_attractionId(request:Request,attractionId:Annotated[int, Path(gt=0)]):
+	cursor=con.cursor()
+	results=getAttractionData(cursor,keyword=None,pageStartFrom=0,attractionId=attractionId)
+	images=getAttractionImg(cursor,attractions_id=attractionId)
+	if results and images:
+		data={
+			"id":results[0][0],
+			"name":results[0][1],
+			"category":results[0][2],
+			"description":results[0][3],
+			"address":results[0][4],
+			"transport":results[0][5],
+			"mrt":results[0][6],
+			"lat":results[0][7],
+			"lng":results[0][8],
+			"images":images
+		}
+		return {"data":data}
+	else:
+		return JSONResponse(status_code=400,content={"error":True,"message":"您所查詢的景點編號不存在。"})
 
 
 # Static Pages (Never Modify Code in this Block)
