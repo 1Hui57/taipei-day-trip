c2e1409fe2b45a193c7afeaf6f861b87d4b3088f
1Hui57 <melody870507@gmail.com>
2025-03-21 14:47:54 +0800

    feat:j完成登入/註冊跳出視窗

--- taipei-day-trip/public/dialog.js
+++ taipei-day-trip/public/dialog.js
@@ -1,14 +1,36 @@
 let signinBtn = document.getElementById("signinBtn");
 signinBtn.addEventListener('click',function(){
-    let dialogSection = document.getElementById("dialog-section");
-    dialogSection.classList.remove("display-none");
+    let dialogSectionSignin = document.getElementById("dialog-section--signin");
+    dialogSectionSignin.classList.remove("display-none");
 })
 
-let dialogCloseBtn =document.getElementById("dialog-close-btn");
-
-dialogCloseBtn.addEventListener('click',function(){
-    console.log("成功點");
-    let dialogSection = document.getElementById("dialog-section");
-    dialogSection.classList.add("display-none");
+let dialogCloseBtnSignIn =document.getElementById("dialog-close-btn-signin");
+dialogCloseBtnSignIn.addEventListener('click',function(){
+    let dialogSectionSignin = document.getElementById("dialog-section--signin");
+    dialogSectionSignin.classList.add("display-none");
 })
+
+let dialogCloseBtnSignUp =document.getElementById("dialog-close-btn-signup");
+dialogCloseBtnSignUp.addEventListener('click',function(){
+    let dialogSectionSignup = document.getElementById("dialog-section--signup");
+    dialogSectionSignup.classList.add("display-none");
+})
+
+let signInSectionSignUpBtn = document.getElementById("signInSection-signUpBtn");
+signInSectionSignUpBtn.addEventListener('click', function(){
+    let dialogSectionSignin = document.getElementById("dialog-section--signin");
+    dialogSectionSignin.classList.add("display-none");
+    let dialogSectionSignup = document.getElementById("dialog-section--signup");
+    dialogSectionSignup.classList.remove("display-none");
+})
+
+let signUpSectionSignUpBtn = document.getElementById("signUpSection-signInBtn");
+signUpSectionSignUpBtn.addEventListener('click', function(){
+    let dialogSectionSignup = document.getElementById("dialog-section--signup");
+    dialogSectionSignup.classList.add("display-none");
+    let dialogSectionSignin = document.getElementById("dialog-section--signin");
+    dialogSectionSignin.classList.remove("display-none");
+})
+
+
 
